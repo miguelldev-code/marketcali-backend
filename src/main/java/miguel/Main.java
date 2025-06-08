eb5e0f532b1cc127d45bb587a9d63b1f6898d3cc
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-08 00:05:03 -0500

    Prueba de datos en el main para ejecutar el DAO

--- src/main/java/miguel/Main.java
+++ src/main/java/miguel/Main.java
@@ -1,7 +1,28 @@
 package miguel;
+import miguel.marketcali.dao.ProductoDAO;
+import miguel.marketcali.model.Producto;
+
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        ProductoDAO dao = new ProductoDAO();
+
+        // Crear producto
+        Producto nuevo = new Producto("Laptop", "Dell", 2500000, 10, "Tecnología");
+        dao.insertarProducto(nuevo);
+
+        // Consultar producto
+        Producto consultado = dao.consultarProducto(1);
+        System.out.println("Producto: " + consultado.getNombre());
+
+        // Actualizar producto
+        consultado.setPrecio(2300000);
+        dao.actualizarProducto(consultado);
+
+        // Eliminar producto
+        dao.eliminarProducto(1);
+
     }
 }
