a56d694afa867e5681702c1904f713303dcac5bd
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-09 21:37:06 -0500

    Eliminar productos por medio del servlet

--- src/main/java/miguel/marketcali/servlets/EliminarProductoServlet.java
+++ src/main/java/miguel/marketcali/servlets/EliminarProductoServlet.java
@@ -1,4 +1,23 @@
 package miguel.marketcali.servlets;
 
-public class EliminarProductoServlet {
+import miguel.marketcali.dao.ProductoDAO;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "EliminarProductoServlet", urlPatterns = {"/eliminarProducto"})
+public class EliminarProductoServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        try {
+            new ProductoDAO().eliminarProducto(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        response.sendRedirect("productos");
+    }
 }
