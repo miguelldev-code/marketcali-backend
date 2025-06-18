ec0f5a54c5ffdab70dadde3ec29d00311d6077df
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-17 20:28:05 -0500

    Ajustando Main sin contenido necesario

--- src/main/java/miguel/Main.java
+++ src/main/java/miguel/Main.java
@@ -12,31 +12,4 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @Bean
-    public CommandLineRunner demo(ProductoRepository repository) {
-        return (args) -> {
-            // Create product
-            Producto nuevo = new Producto("Laptop", "Dell", 2500000, 10, "Tecnología");
-            repository.save(nuevo);
-            System.out.println("Producto creado");
-
-            // Query product
-            Producto consultado = repository.findById(1L).orElse(null);
-            if (consultado != null) {
-                System.out.println("Producto encontrado: " + consultado.getNombre());
-
-                // Update product
-                consultado.setPrecio(2300000);
-                repository.save(consultado);
-                System.out.println("Producto actualizado");
-
-                // Delete product
-                repository.deleteById(1L);
-                System.out.println("Producto eliminado");
-            } else {
-                System.out.println("Producto no encontrado");
-            }
-        };
-    }
 }
