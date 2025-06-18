ecd8b888ceab5ae40a6b25cff0b32a6db254ad9c
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-17 20:33:45 -0500

    Repositorio productos con listar producto (categoria y precio)

--- src/main/java/miguel/marketcali/repository/ProductoRepository.java
+++ src/main/java/miguel/marketcali/repository/ProductoRepository.java
@@ -2,6 +2,20 @@ package miguel.marketcali.repository;
 
 import miguel.marketcali.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findByCategoria(String categoria);
+    List<Producto> findByMarca(String marca);
+    List<Producto> findByCategoriaAndMarca(String categoria, String marca);
+
+    // NO IMPLEMENTADO EN REACT
+    // Filtros de categorias y marcas
+    @Query("SELECT DISTINCT p.categoria FROM Producto p WHERE p.categoria IS NOT NULL")
+    List<String> findDistinctCategorias();
+
+    @Query("SELECT DISTINCT p.marca FROM Producto p WHERE p.marca IS NOT NULL")
+    List<String> findDistinctMarcas();
 }
