3c5ef614524d175a080056f1b75781d5bb727360
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-06-25 09:22:05 -0500

    Borrando importaciones no usadas

--- src/main/java/miguel/Main.java
+++ src/main/java/miguel/Main.java
@@ -1,11 +1,7 @@
 package miguel;
 
-import miguel.marketcali.model.Producto;
-import miguel.marketcali.repository.ProductoRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
