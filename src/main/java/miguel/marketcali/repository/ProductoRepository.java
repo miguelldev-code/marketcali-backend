3cf28b0721cd337cff3788a4ad314aa11295a62a
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-07-18 00:16:30 -0500

    Repositorio para buscar por codigo de barras y verificar que exista el codigo de barras registrado

--- src/main/java/miguel/marketcali/repository/ProductoRepository.java
+++ src/main/java/miguel/marketcali/repository/ProductoRepository.java
@@ -2,17 +2,11 @@ package miguel.marketcali.repository;
 
 import miguel.marketcali.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
+import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    Optional<Producto> findByCodigoBarras(String codigoBarras);
 
-    // NO IMPLEMENTADO EN REACT
-    // Filtros de categorias y marcas
-    @Query("SELECT DISTINCT p.categoria FROM Producto p WHERE p.categoria IS NOT NULL")
-    List<String> findDistinctCategorias();
-
-    @Query("SELECT DISTINCT p.marca FROM Producto p WHERE p.marca IS NOT NULL")
-    List<String> findDistinctMarcas();
+    // Añade este método
+    boolean existsByCodigoBarras(String codigoBarras);
 }
